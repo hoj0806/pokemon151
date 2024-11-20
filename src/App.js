9545d117d47a8e8a9002cbd3b61ce0788c05ef2d
홍재훈 <ehswkfn123@naver.com>
2024-11-20 22:43:28 +0900

    docs : global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <header className='navbar'>asd</header>
+    </div>
+  );
 }
 
 export default App;

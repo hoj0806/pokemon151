7ef93db07a9890211c6cc2c75f88c66762875ae1
홍재훈 <ehswkfn123@naver.com>
2024-11-22 00:06:55 +0900

    ui : navbar 기본 디자인

--- src/App.js
+++ src/App.js
@@ -7,35 +7,33 @@ import PokemonList from "./components/PokemonList/PokemonList";
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
 
+  const fetchAllPokemonData = async () => {
+    const allPokemonData = [];
+    for (let i = 1; i <= 40; i++) {
+      // image Reponse
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const data = await response.json();
+      const frontSpriteImage = data.sprites.front_default;
+
+      // name Response
+      const speciesResponse = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${i}`
+      );
+      const speciesData = await speciesResponse.json();
+
+      const koreanName = speciesData.names.find(
+        (el) => el.language.name === "ko"
+      ).name;
+      allPokemonData.push({
+        imageUrl: frontSpriteImage,
+        name: koreanName,
+        id: speciesData.id,
+      });
+    }
+    setPokemonData(allPokemonData);
+  };
+
   useEffect(() => {
-    const fetchAllPokemonData = async () => {
-      const allPokemonData = [];
-      for (let i = 1; i <= 40; i++) {
-        // const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-
-        // image Reponse
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-        const data = await response.json();
-        const frontSpriteImage = data.sprites.front_default;
-
-        // name Response
-        const speciesResponse = await fetch(
-          `https://pokeapi.co/api/v2/pokemon-species/${i}`
-        );
-        const speciesData = await speciesResponse.json();
-
-        const koreanName = speciesData.names.find(
-          (el) => el.language.name === "ko"
-        ).name;
-        allPokemonData.push({
-          imageUrl: frontSpriteImage,
-          name: koreanName,
-          id: speciesData.id,
-        });
-      }
-      setPokemonData(allPokemonData);
-    };
-
     fetchAllPokemonData();
   }, []);
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,13 @@
 import "./Navbar.css";
 
 export default function Navbar() {
-  return <header className='navbar'>ds</header>;
+  return (
+    <header className='navbar'>
+      <h1>Poke151</h1>
+      <input
+        className='search-input'
+        placeholder='포켓몬을 검색해보세요!'
+      ></input>
+    </header>
+  );
 }
